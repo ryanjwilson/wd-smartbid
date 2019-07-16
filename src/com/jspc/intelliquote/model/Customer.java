34fc2e8409d86045311014904ed95f373ca39a60
Ryan Wilson <xryanwilson@gmail.com>
2019-07-16 14:04:44 -0400

    removed getters/setters

--- src/com/jspc/intelliquote/model/Customer.java
+++ src/com/jspc/intelliquote/model/Customer.java
@@ -15,25 +15,9 @@ public class Customer {
 		this.email = email;
 		this.phone = phone;
 	}
-		
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
 	
 	public Address getAddress() {
 		return address;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getPhone() {
-		return phone;
 	}
 	
 	public String getFormattedName() {
